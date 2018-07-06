86f51947da70286e06ec3944ae39c4749613f887
Matt Payne <Payne@MattPayne.org>
2018-07-05 21:29:29 -0500

    Punch and run with p5-manager.

---
+++ project1/sketch.js
@@ -0,0 +1,17 @@
+var maxCircleSize = 20
+var phase = 0, speed = 0.03;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(127);
+  var x = width/2;
+  var y = height/2 + sin(phase) * 50;
+  phase = frameCount * speed;
+  var sizeOffset = (cos(phase) + 1) * 0.5;
+  var circleSize = sizeOffset * maxCircleSize;
+  
+  ellipse(x, y, circleSize, circleSize);
+}
